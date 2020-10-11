001d962bb182a48bcd8f0df782c70ffbc7e5ffc3
Héla Ben Khalfallah <helabenkhalfallah@MacBook-Pro.local>
2020-10-11 20:51:48 +0200

    [Feature] jest config

---
+++ config/jest/jestTransformer.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+module.exports = require('babel-jest').createTransformer({
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+
+  plugins: [
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        legacy: true,
+      },
+    ],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true,
+      },
+    ],
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-regenerator',
+    '@babel/plugin-transform-modules-commonjs',
+  ],
+});
