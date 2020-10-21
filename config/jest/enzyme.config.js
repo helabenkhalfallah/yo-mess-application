c288302d23f56ea0c6ec3ca0d8c32c65bc795e3d
Héla Ben Khalfallah <helabenkhalfallah@MacBook-Pro.local>
2020-10-21 21:15:22 +0200

    [Feature] upgrade react to 17 version

--- config/jest/enzyme.config.js
+++ config/jest/enzyme.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { configure, } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-configure({ adapter: new Adapter(), });
+Enzyme.configure({
+  adapter: new Adapter(),
+});
